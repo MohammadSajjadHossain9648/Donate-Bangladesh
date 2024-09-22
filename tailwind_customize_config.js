a246a695041a473c0e4408fac4d7509d32e3e9ce
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-09-22 23:01:58 +0600

    Implement JS DOM manipulation for header section buttons

--- tailwind_customize_config.js
+++ tailwind_customize_config.js
@@ -5,10 +5,10 @@ tailwind.config = {
       extend: {
         colors: {
           green_color: 'rgb(180, 244, 97)',
-          black_color1: 'rgb(17, 17, 17)',
+          black_color: 'rgb(17, 17, 17)',
           dark_shade_color: 'rgba(17, 17, 17, 0.7)',
           light_shade_color: 'rgba(17, 17, 17, 0.4)',
-          bg_color: 'rgb(249, 247, 243)',
+          shade_white_color: 'rgb(249, 247, 243)',
         }
       }
     }
