a4d4375139efcfa002589ab20f8b72d07e1a675f
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-09-22 20:52:57 +0600

    Initial project setup done

---
+++ tailwind_customize_config.js
@@ -0,0 +1,15 @@
+// customize configuration with own design tokens
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          green_color: 'rgb(180, 244, 97)',
+          black_color1: 'rgb(17, 17, 17)',
+          dark_shade_color: 'rgba(17, 17, 17, 0.7)',
+          light_shade_color: 'rgba(17, 17, 17, 0.4)',
+          bg_color: 'rgb(249, 247, 243)',
+        }
+      }
+    }
+  }
